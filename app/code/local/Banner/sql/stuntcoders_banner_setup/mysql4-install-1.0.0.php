92d38ea3e31fea26e30b829d3ce0da8feb67415a
Mladen Ilic <mladen@stuntcoders.com>
2015-02-25 11:08:33 +0100

    Refactor install scipts

--- app/code/local/Banner/sql/stuntcoders_banner_setup/mysql4-install-1.0.0.php
+++ app/code/local/Banner/sql/stuntcoders_banner_setup/mysql4-install-1.0.0.php
@@ -6,17 +6,28 @@ $installer->startSetup();
 
 $installer->run("
 DROP TABLE IF EXISTS `{$this->getTable('stuntcoders_banner/banner')}`;
+DROP TABLE IF EXISTS `{$this->getTable('stuntcoders_banner/banner_group')}`;
 
 CREATE TABLE `{$this->getTable('stuntcoders_banner/banner')}` (
-    `banner_id` smallint(6) NOT NULL AUTO_INCREMENT,
-    `width` smallint(6) NOT NULL,
-    `height` smallint(6) NOT NULL,
-    `display` smallint(6) DEFAULT 0,
-    `url` varchar(255) DEFAULT '',
-    `image` varchar(255) DEFAULT '',
-    `category` smallint(6) DEFAULT 0,
-    PRIMARY KEY (`banner_id`)
+    `banner_id` SMALLINT(6) NOT NULL AUTO_INCREMENT,
+    `group_id` SMALLINT(6),
+    `code` VARCHAR(255) DEFAULT '',
+    `url` VARCHAR(255) DEFAULT '',
+    `text` VARCHAR(255) DEFAULT '',
+    `image` VARCHAR(255) DEFAULT '',
+    `heading` VARCHAR(255) DEFAULT '',
+    PRIMARY KEY (`banner_id`),
+    UNIQUE KEY STUNTCODERS_BANNER_UNIQUE_CODE (`code`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Banner instance' ;
+
+CREATE TABLE `{$this->getTable('stuntcoders_banner/banner_group')}` (
+    `group_id` smallint(6) NOT NULL AUTO_INCREMENT,
+    `code` varchar(255) NOT NULL,
+    `name` varchar(255) DEFAULT '',
+    PRIMARY KEY (`group_id`),
+    UNIQUE KEY `STUNTCODERS_BANNER_GROUP_UNIQUE_CODE` (`code`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Banner group instance' ;
+
 ");
 
 $installer->endSetup();
