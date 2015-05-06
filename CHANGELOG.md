455fa1a806dc660e38e26a39c4ab076c9b05e9a5
Mladen Ilic <mladen@stuntcoders.com>
2015-05-06 11:44:17 +0200

    Fix merge conflict errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Change Log
 All notable changes to this project will be documented in this file. This project adheres to [Semantic Versioning](http://semver.org/).
 
+## 1.2.1 - 2015-05-06
+### CHANGED:
+- Fix merge conflict errors
+
 ## 1.2.0 — 2015-05-06
 ### ADDED:
 - Adminhtml layout updates
--- app/code/local/Stuntcoders/Banner/Model/Banner.php
+++ app/code/local/Stuntcoders/Banner/Model/Banner.php
@@ -7,11 +7,31 @@ class Stuntcoders_Banner_Model_Banner extends Mage_Core_Model_Abstract
         $this->_init('stuntcoders_banner/banner');
     }
 
+    public function getImage()
+    {
+        $imagePath = $this->getData('image');
+        if (empty($imagePath)) {
+            return "";
+        }
+        return Mage::getBaseUrl('media') . $imagePath;
+    }
+
+    public function getOpenInNewTab()
+    {
+        return (bool) $this->getData('open_in_new_tab');
+    }
+
     public function getBannersByGroupCode($code)
     {
         $bannerGroupModel = Mage::getModel('stuntcoders_banner/banner_group');
         $groupId = $bannerGroupModel->getIdByCode($code);
+        $banners = $bannerGroupModel->load($groupId)->getBanners();
+        $bannerCollection = new Varien_Data_Collection();
 
-        return $bannerGroupModel->load($groupId)->getBanners();
+        foreach ($banners as $banner) {
+            $bannerCollection->addItem(Mage::getModel('stuntcoders_banner/banner')->load($banner['banner_id']));
+        }
+
+        return $bannerCollection;
     }
 }
