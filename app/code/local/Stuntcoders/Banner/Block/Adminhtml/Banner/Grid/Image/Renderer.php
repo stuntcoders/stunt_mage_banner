eb117998507e0b97940d7c263fe15c8147ab7c09
Mladen Ilić <mladen@stuntcoders.com>
2016-12-27 10:23:38 +0100

    Fix banner grid image field

--- app/code/local/Stuntcoders/Banner/Block/Adminhtml/Banner/Grid/Image/Renderer.php
+++ app/code/local/Stuntcoders/Banner/Block/Adminhtml/Banner/Grid/Image/Renderer.php
@@ -7,11 +7,8 @@ class Stuntcoders_Banner_Block_Adminhtml_Banner_Grid_Image_Renderer
     {
         if (empty($row['image'])) {
             return "<div class=\"a-center\">{$this->__('-- No image --')}</div>";
-
         }
 
-        $url = Mage::helper('stuntcoders_banner')->getBaseMediaUrl() . $row['image'];
-
-        return "<img src=\"{$url}\" width=\"100\"/>";
+        return "<img src=\"{$row['image']}\" width=\"100\"/>";
     }
 }
