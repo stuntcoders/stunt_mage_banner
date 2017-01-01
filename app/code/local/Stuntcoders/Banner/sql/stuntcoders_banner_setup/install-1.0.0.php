a3a7041bb3175a82e3d284e93fd8246c44c2a2ac
Mladen Ilic <mladen@stuntcoders.com>
2017-01-01 17:38:33 +0100

    Save banner store relation

--- app/code/local/Stuntcoders/Banner/sql/stuntcoders_banner_setup/install-1.0.0.php
+++ app/code/local/Stuntcoders/Banner/sql/stuntcoders_banner_setup/install-1.0.0.php
@@ -6,10 +6,10 @@ $this->startSetup();
 $table = $this->getConnection()
     ->newTable($this->getTable('stuntcoders_banner/banner'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'identity'  => true,
-        'unsigned'  => true,
-        'nullable'  => false,
-        'primary'   => true,
+        'identity' => true,
+        'unsigned' => true,
+        'nullable' => false,
+        'primary' => true,
     ))->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null)
     ->addColumn('code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array())
     ->addColumn('url', Varien_Db_Ddl_Table::TYPE_TEXT, null, array())
@@ -25,25 +25,16 @@ $table->addIndex(
     Varien_Db_Adapter_Interface::INDEX_TYPE_UNIQUE
 );
 
-$table->addForeignKey(
-    $this->getFkName('stuntcoders_banner/banner', 'group_id', 'stuntcoders_banner/banner_group', 'id'),
-    'group_id',
-    'stuntcoders_banner/banner_group',
-    'id',
-    Varien_Db_Ddl_Table::ACTION_SET_NULL,
-    Varien_Db_Ddl_Table::ACTION_CASCADE
-);
-
 $this->getConnection()->createTable($table);
 
 $table = $this->getConnection()
     ->newTable($this->getTable('stuntcoders_banner/banner_group'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'identity'  => true,
-        'unsigned'  => true,
-        'nullable'  => false,
-        'primary'   => true,
-    ))->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null)
+        'identity' => true,
+        'unsigned' => true,
+        'nullable' => false,
+        'primary' => true,
+    ))
     ->addColumn('code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array())
     ->addColumn('name', Varien_Db_Ddl_Table::TYPE_TEXT, null, array());
 
